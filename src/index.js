1c24b8ebe82582bcf043385d9ed38902cc27a9bf
Dimasv6s <ramzicod4@gmail.com>
2022-02-25 01:13:53 +0300

    feat: copleted task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracket = []
+  const closeBrackets = [].concat(...bracketsConfig).filter((_, i) => (i % 2))
+  const openBrackets = [].concat(...bracketsConfig).filter((_, i) => !(i % 2))
+  for (let i = 0; i < str.length; i++) {
+    if(openBrackets.includes(str[i]) && closeBrackets.includes(str[i])) {
+      if (bracket[bracket.length - 1] === str[i]) {
+        bracket.pop()
+        continue
+      }
+      bracket.push(str[i])
+      continue
+    }
+    if (openBrackets.includes(str[i])) {
+      bracket.push(str[i])
+      continue
+    }
+    if(str[i] === closeBrackets[openBrackets.indexOf(bracket[bracket.length - 1])]) {
+      bracket.pop()
+    } else { return false
+    }
+  }
+  return bracket.length ? false : true 
 }
